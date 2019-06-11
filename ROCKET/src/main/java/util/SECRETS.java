46a9bd1efda9cd00f9b6f5b6b10b913420f452e8
Sv33n <51695668+Sv33n@users.noreply.github.com>
2019-06-11 09:26:35 +0200

    Update SECRETS.java

--- ROCKET/src/main/java/util/SECRETS.java
+++ ROCKET/src/main/java/util/SECRETS.java
@@ -4,7 +4,7 @@ package util;
 public class SECRETS {
 
 
-    public static String TOKEN = "NTY2OTc5NjI3MTU5OTEyNDc2.XLM3Dw.HVClVAnXExzWrN6JJVPEAqfiTv8";
+    public static String TOKEN = "---";
 
 
 
