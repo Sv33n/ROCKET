a23bcad1f34cf7c10ad69f97492faae8891b0f62
Sv33n <51695668+Sv33n@users.noreply.github.com>
2020-01-03 11:43:44 +0100

    Update STATIC.java

--- ROCKET/src/main/java/util/STATIC.java
+++ ROCKET/src/main/java/util/STATIC.java
@@ -4,18 +4,11 @@ import java.util.Date;
 
 public class STATIC {
 
-    //NEUER TOKEN??
-    //TOKEN IN MAIN?!
-    //BOTNAME??
-    //VERSION??
-    //PREFIX??
 
-
-    public static final String BOTNAME = "IceBot";
+    public static final String BOTNAME = "ROCKET";
     public static final String BOTOWNER = "Sv3n#0628";
     public static final String DEVELOPER = "Sv3n#0628";
     public static final String OWNERID = "275714969973161984";
-    public static final String PING = "23ms";
     public static final String VERSION = "v. 1.0";
     public static final String PREFIX = "!";
     public static final String SupportServer = "https://discord.gg/UPM7KkB";
@@ -25,21 +18,6 @@ public class STATIC {
     public static final String[] PERMISSION = {"P.Moderator","Moderator", "Manager", "Administrator"};
     public static final String[] OWNER = {"275714969973161984"};
 
-
-    //------------------------ MUSIC -----------------------------//
-
-    public static int music_volume = -10;
-    public static String input;
-    public static String musicChannel = "#serverlog";
-
-    //------------------------ LOG -------------------------------//
-
-    public static String Zeitzone = " [UTC +1]";
-    public static Date lastRestart;
-    public static int reconnectCount = 0;
-
-
-    //----------------------- GAME -------------------------------//
 
 }
 
